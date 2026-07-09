ae5c474d1580b5e2a1238fd68b8f578a15b6afe0
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2026-07-10 00:00:11 +0200

    .

--- src/main/java/de/htwberlin/webtech/WebtechApplication.java
+++ src/main/java/de/htwberlin/webtech/WebtechApplication.java
@@ -1,7 +1,7 @@
 package de.htwberlin.webtech;
 
 public class WebtechApplication {
-	public static void main(String[] args) {
-		io.quarkus.runtime.Quarkus.run(args);
-	}
+    public static void main(String[] args) {
+        io.quarkus.runtime.Quarkus.run(args);
+    }
 }
